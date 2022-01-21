32b94762fa3ddec2a291223002586490a1616d4b
Hugo <hpenacho89@gmail.com>
2022-01-21 21:07:09 +0000

    removed duplicate image

--- src/components/Header/AboutThisProject.jsx
+++ src/components/Header/AboutThisProject.jsx
@@ -3,46 +3,37 @@ import { DialogContentText } from '@mui/material';
 import { Divider } from '@mui/material';
 import { Link } from '@mui/material';
 import { Grid } from '@mui/material';
-import Image from 'mui-image'
-import photo from '../../assets/images/face1.jpg'
 
 const AboutThisProject = {
     title: 'About this project',
     icon: <LibraryBooksIcon />,
     content: <Grid container px={1}>
-        <Grid container justifyContent='center'>
+        <Grid container py={1.5} justifyContent='center'>
             <DialogContentText mb={1.6} variant='h6' id="aboutme-intro">
                 Solaris Weather App, my first project in React.js <br />
             </DialogContentText>
         </Grid>
-
-        <Grid container justifyContent={'space-between'} >
-            <Grid item md={7.2} xs={12} order={{ xs: 2, sm: 2, md: 1 }}>
-                <b>Purpose</b>
-                <Divider />
-                <DialogContentText mb={1.5} variant='body2' id="aboutme-academics">
-                    I first decided to create this web-app after finishing the University of Helsinki MERN Stack course.  <br />
-                    This was an opportunity to develop a project where I could apply what I learned in a more practical manner beyond the isolated exercises proposed by the course.
-                </DialogContentText>
-
-                <b>Design Patterns</b>
-                <Divider />
-                <DialogContentText mb={1.5} variant='body2' id="aboutme-career">
-                    Whenever possible, I tried to maintain a fair level of abstraction towards my components, such that they could be reused without duplicating code. <br />
-                    In an attempt to make the code tidier and more easily readable, I separated component logic from UI whenever practical. <br />
-                    React hooks were used abundantly, although for my next projects I plan to use Redux for easier state management. <br />
-                    API calls are kept to a minimum.
-                </DialogContentText>
-            </Grid>
-            <Grid item md={4} xs={12} my={3} order={{ xs: 1, sm: 1, md: 2 }}>
-                <Image
-                    src={photo}
-                    style={{ borderRadius: 300, maxWidth: 220, maxHeight: 220 }}
-                    duration={4000}
-                />
-            </Grid>
+        <Grid item xs={9}>
+            <b>Purpose</b>
+            <Divider />
+            <DialogContentText mb={1.5} variant='body2' id="aboutme-academics">
+                I first decided to create this web-app after finishing the University of Helsinki MERN Stack course.  <br />
+                This was an opportunity to develop a project where I could apply what I learned in a more practical manner beyond the isolated exercises proposed by the course.
+            </DialogContentText>
         </Grid>
-        <Grid item>
+        <Grid item xs={9}>
+            <b>Design Patterns</b>
+            <Divider />
+            <DialogContentText mb={1.5} variant='body2' id="aboutme-career">
+                Whenever possible, I tried to maintain a fair level of abstraction towards my components, such that they could be reused without duplicating code. <br />
+                In an attempt to make the code tidier and more easily readable, I separated component logic from UI whenever practical. <br />
+                React hooks were used abundantly, although for my next projects I plan to use Redux for easier state management. <br />
+                API calls are kept to a minimum.
+            </DialogContentText>
+        </Grid>
+
+
+        <Grid item xs={9}>
             <b>More about this project</b>
             <Divider />
             <DialogContentText mb={1.5} variant='body2' id="aboutme-hobbies">
