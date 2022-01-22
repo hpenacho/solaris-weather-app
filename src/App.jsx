1b97fad2100542e2da933e0646a16fb51e07d77f
Hugo <hpenacho89@gmail.com>
2022-01-22 01:33:56 +0000

    development

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { SnackbarProvider } from 'notistack';
 import darkTheme from './assets/darkTheme'
 import { ThemeProvider } from '@mui/material/styles'
 import { CssBaseline } from '@mui/material';
-//import WeatherApp from './components/WeatherApp'
+import WeatherApp from './components/WeatherApp'
 import HeaderSection from './components/Header/HeaderSection'
 import FooterSection from './components/Footer/FooterSection'
 import { Grid } from '@mui/material';
@@ -11,7 +11,7 @@ import { Grid } from '@mui/material';
 const App = () => {
   const [theme, setTheme] = useState(darkTheme)
 
-  //<WeatherApp />
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline enableColorScheme />
@@ -23,7 +23,7 @@ const App = () => {
           minHeight="100vh"
         >
           <HeaderSection theme={theme} setTheme={setTheme} />
-          test
+          <WeatherApp />
           <FooterSection />
         </Grid>
       </SnackbarProvider>
